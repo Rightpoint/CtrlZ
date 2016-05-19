2b4347de41b46f30a7b3b94d40a6491689d9b4f4
Eric Slosser <eric.slosser@raizlabs.com>
2016-05-19 15:54:28 -0400

    delete all but README from master

--- Example/Client/Pods/Target Support Files/Pods-CtrlZ-Example-CtrlZ/Pods-CtrlZ-Example-CtrlZ-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CtrlZ_Example_CtrlZ : NSObject
-@end
-@implementation PodsDummy_Pods_CtrlZ_Example_CtrlZ
-@end
--- Example/Client/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  CtrlZTests.m
-//  CtrlZTests
-//
-//  Created by spencer poff on 02/20/2015.
-//  Copyright (c) 2014 spencer poff. All rights reserved.
-//
-
-${TEST_EXAMPLE}
